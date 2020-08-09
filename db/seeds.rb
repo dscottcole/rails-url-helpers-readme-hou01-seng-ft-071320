eaf0e6c4cb2e6040117fac3834ad801ff4d4ace4
dscottcole <67556122+dscottcole@users.noreply.github.com>
2020-08-09 13:43:14 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create(title: "E-bikes are lame", description: "1000 reasons why e-bikes suck and will make you lame")
